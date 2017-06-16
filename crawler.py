008ea489df0de6151e981b8696665cf0ab6526a9
Nurtal <nathan.foulquier.pro@gmail.com>
2017-06-16 14:34:46 +0200

    fix

--- crawler.py
+++ crawler.py
@@ -11,18 +11,26 @@ def gather_doc_rules():
 	[IN PROGRESS]
 	"""
 
+
 	rules = rules_manager.get_all_rules_from_file("HLA_phase1_all_rules_filtered.txt")
+	cmpt = 0
 	for rule in rules.values():
+
+		result_file = open("results/abstract_for_rule_"+str(cmpt)+".txt", "w")
 		query = rules_manager.generate_querry_from_rule(rule)
 		pmid_list = bibliosearch.get_ListOfArticles(query, 1500)
-		print pmid_list
 		for pmid in pmid_list:
 			try:
 				test = bibliosearch.fetch_abstract(pmid)
-				print "["+str(pmid)+"]\n"
+				print "[+] Read "+str(pmid)
 				print test
+				result_file.write(">PMID;"+str(pmid)+"\n")
+				result_file.write(test+"\n")
 			except:
+				result_file.write("[!] Can't read "+str(pmid)+"\n")
 				print "[!] Can't read "+str(pmid)
+		result_file.close()
+
 
 ### TEST SPACE ###
 gather_doc_rules()
